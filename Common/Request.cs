bbb2847e8da0eb02b651babbd07603bf6e1d1373
marijak95 <marija.k.95@gmail.com>
2017-11-20 11:44:24 +0100

    base

---
+++ Common/Request.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Request
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int UserId { get; set; }
+        public bool Approved { get; set; }
+    }
+}
--- Common/User.cs
+++ Common/User.cs
@@ -11,7 +11,23 @@
         public string QuestionTwo { get; set; }
         public string AnswerOne { get; set; }
         public string AnswerTwo { get; set; }
-        public bool LoggedIn { get; set; }
+       // public bool LoggedIn { get; set; }
         public int BossId { get; set; }
+        public string Group { get; set; }
+
+
+        public User(string username, string firstName, string lastName, string password, string questionOne, string questionTwo, string answerOne, string answerTwo)
+        {
+            this.Username = username;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Password = password;
+            this.QuestionOne = questionOne;
+            this.QuestionTwo = questionTwo;
+            this.AnswerOne = answerOne;
+            this.AnswerTwo = answerTwo;
+            this.BossId = 0;
+            this.Group = null;
+        }
     }
 }
